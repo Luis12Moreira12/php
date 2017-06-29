31776b9bd6309f3156dac194296fd2b21bd9dbdb
Luis <lberama@gmail.com>
2017-06-29 16:34:19 -0300

    adicionei mais quatro echos

--- index.php
+++ index.php
@@ -4,4 +4,8 @@
 		echo "Olá Udemy";
 		echo "programar´´e o máximo";
 		echo "programe você também!";
+		echo "meu nome é Luis";
+		echo "minha esposa é Lara";
+		echo "minha filha é a brigitte!";
+		echo "eu amo minha familia!";
 ?>
