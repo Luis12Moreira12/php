0c48989d48cc29a53cc309db818577e39e16d8f6
Luis <lberama@gmail.com>
2017-06-29 16:35:49 -0300

    mantive apenas os echos iniciais

--- index.php
+++ index.php
@@ -2,10 +2,5 @@
 
 		echo"Olá Mundo 456!";
 		echo "Olá Udemy";
-		echo "programar´´e o máximo";
-		echo "programe você também!";
-		echo "meu nome é Luis";
-		echo "minha esposa é Lara";
-		echo "minha filha é a brigitte!";
-		echo "eu amo minha familia!";
+		
 ?>
